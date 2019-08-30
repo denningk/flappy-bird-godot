cc0357ef36b27490ead5a89466d1edc99cf9a898
Keith <keithdenning1@gmail.com>
2019-08-30 18:27:35 -0400

    destroyed off screen grounds

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -9,3 +9,6 @@ func _ready():
 	
 func _physics_process(delta):
 	position = Vector2(bird.position.x, position.y)
+	
+func get_total_pos():
+	return position + offset
--- scripts/Ground.gd
+++ scripts/Ground.gd
@@ -2,9 +2,17 @@
 
 extends StaticBody2D
 
+onready var bottom_right = $Bottom_right
+onready var camera = Utils.get_main_node().get_node("Camera")
+
+signal destroyed
+
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	#if position.x + 168 
-	pass
+	if camera == null: return
+	
+	if bottom_right.global_position.x <= camera.get_total_pos().x:
+		queue_free()
+		emit_signal("destroyed")
