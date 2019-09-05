59a8a3d9f624ffb513e51da58ff123fab166eaa5
Keith Denning <keithdenning1@gmail.com>
2019-09-05 17:54:39 -0400

    destroy pipes when they leave the screen

--- scripts/Pipe.gd
+++ scripts/Pipe.gd
@@ -4,5 +4,7 @@ onready var right : Position2D = $Right
 onready var camera : Camera2D = Utils.get_main_node().get_node("Camera")
 
 func _process(_delta):
-	if right.global_position.x:
-		pass
+	if camera == null: return
+	
+	if right.global_position.x <= camera.get_total_pos().x:
+		queue_free()
